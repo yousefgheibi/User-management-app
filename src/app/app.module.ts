159ccebb33e8c4a5adce17d462c6d7fda59ed92d
Yousef <gheibiyousef2000@gmail.com>
2022-02-09 21:34:09 +0330

    create base-template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ShowUserComponent } from './show-user/show-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    ShowUserComponent
   ],
   imports: [
     BrowserModule
